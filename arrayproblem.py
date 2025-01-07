6249f0741280a2f4cff6d73e4395ca940aad346d
Akzhol <146413018+nuketayev@users.noreply.github.com>
2025-01-07 10:59:56 +0000

    playground init np

---
+++ arrayproblem.py
@@ -0,0 +1,26 @@
+# Initialize an array as below:
+# [1,1,1,1,1]
+# [1,0,0,0,1]
+# [1,0,9,0,1]
+# [1,0,0,0,1]
+# [1,1,1,1,1]
+import numpy as np
+
+one = np.ones((5,5))
+print("Ones: ")
+print(one)
+print()
+
+zeros = np.zeros((3,3))
+print("Zeros: ")
+print(zeros)
+print()
+
+zeros[1,1] = 9
+print("Zeros with nine: ")
+print(zeros)
+print()
+
+one[1:4,1:4] = zeros
+print("Result: ")
+print(one)
---
+++ initnp.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+a = np.zeros((4,3,2))
+# print(a)
+
+b = np.ones((3,4,5), dtype='int32')
+# print(b)
+
+b = a.copy() # copying content
+b = a # poiting to the same address as a, thus any change on B will influence a.
+
+c = np.full((3,3,3), 99, dtype='float32')
+# print(c)
+
+k = np.full_like(a, 34) # np.full(a.shape, 34)
+# print("k: ",k)
+
+j = np.random.rand(3,2,4)
+# print(j)
+j = np.random.random_sample(a.shape)
+# print(j)
+j = np.random.randint(-1, 3, size=a.shape)
+# print(j)
+
+# Martix identity
+j = np.identity(3)
+# print(j)
+
+arr = np.array([[[1,2,3]]])
+p = np.repeat(arr, 3, axis=2)
+print(p)
