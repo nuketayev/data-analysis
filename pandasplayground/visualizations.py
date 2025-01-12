bd3d54bc3438623081d610163114655e78148517
nuketayev <akzhol.nuketayev@gmail.com>
2025-01-12 15:13:55 +0100

    viz

--- pandasplayground/visualizations.py
+++ pandasplayground/visualizations.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 x = np.arange(-10,11)
 
 plt.figure(figsize=(12, 6))
-
+plt.subplot(1,2,1)
 plt.title('The plot')
 plt.plot(x, x**2)
 plt.plot(x, -1 * (x**2))
 plt.show()
 
-plt.subplot(1,2,1)
